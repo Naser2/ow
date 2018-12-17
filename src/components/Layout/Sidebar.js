dfe178d5faa79c87e66840773a7137c11995c14f
Nas <nassersanou23@gmail.com>
2018-12-17 14:42:11 -0500

    Wip Layou-3

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 export default function Sidebar() {
   return (
     <div>
-      
+      <Link to="client/add" className="btn btn-success btn-block"></Link>
+     <i className="Fas fa-plus" /> New 
     </div>
-  )
+  );
 }
