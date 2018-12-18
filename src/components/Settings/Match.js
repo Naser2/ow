1bf9dbb3cc4eb5aef2748192a271c5d4a110eeb1
Nas <nassersanou23@gmail.com>
2018-12-18 01:53:53 -0500

    Add Match component WIP

---
+++ src/components/Settings/Match.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Match extends Component {
+  render() {
+    return (
+      <div>
+        Close Matches Found
+      </div>
+    )
+  }
+}
