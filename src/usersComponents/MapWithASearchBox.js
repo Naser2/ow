19d7c9c4f2e8a69c9ff229b1df98d8aad12796e8
Nas <nassersanou23@gmail.com>
2018-12-19 04:28:13 -0500

    adding LandingPage modified

--- src/usersComponents/MapWithASearchBox.js
+++ src/usersComponents/MapWithASearchBox.js
@@ -12,7 +12,7 @@ const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox
 
  const MapWithASearchBox = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=GetApikey()&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBfWSuSgALd753xnz2wi3i0fZ8TJ8WYtHY&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
