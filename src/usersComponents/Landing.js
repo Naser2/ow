58a8b7fdc0f3f7115d2d933dcf09d249240fa8d2
Nas <nassersanou23@gmail.com>
2018-12-18 21:32:39 -0500

    Add Search bar and map on landing

--- src/usersComponents/Landing.js
+++ src/usersComponents/Landing.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import Search from '../Search'
 import Match from '../Match'
 import {PlacesWithStandaloneSearchBox} from './StandAloneSearch';
+import MapWithASearchBox from './MapWithASearchBox';
 export default class Landing extends Component {
   render() {
     return (
       <div>
         <h1>Landing Page</h1>
-        {/* <Search /> */}
-        <PlacesWithStandaloneSearchBox />
+        <MapWithASearchBox />
+        {/* <PlacesWithStandaloneSearchBox /> */}
         <Match />
       </div>
     )
