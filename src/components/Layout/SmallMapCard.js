8befae25ec7b79a4aa1465352d944b78c4e2c095
Nas <nassersanou23@gmail.com>
2018-12-24 03:19:18 -0500

    add after merge conflict resolved

--- src/components/Layout/SmallMapCard.js
+++ src/components/Layout/SmallMapCard.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react'
 export default class SmallMapCard extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div class="ui cards" style={{float:"left", paddingLeft: "1.58571429em"}}><div class="ui card"><div class="content"><img src="#"
-=======
       <div class="ui cards" style={{float:"left",  paddingLeft: "1.58571429em"}}><div class="ui card"><div class="content"><img src="#"
->>>>>>> user-profile-WIP
        class="ui mini right floated image"/><div class="header">your current location</div><div class="meta">share with loved ones or rate</div><div class="description">Here is the map <strong>dont know</strong></div></div><div class="extra content"><div class="ui two buttons"><button class="ui green basic button">share</button><button class="ui red basic button">rate</button>
       </div>
       </div></div>
