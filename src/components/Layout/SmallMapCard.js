6936635ad493a7136c342b255809268080aa0c71
Nas <nassersanou23@gmail.com>
2018-12-24 02:29:08 -0500

    adjust style

--- src/components/Layout/SmallMapCard.js
+++ src/components/Layout/SmallMapCard.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 export default class SmallMapCard extends Component {
   render() {
     return (
-      <div class="ui cards"><div class="ui card"><div class="content"><img src="https://react.semantic-ui.com/images/avatar/large/steve.jpg" class="ui mini right floated image"/><div class="header">Your location</div><div class="meta">Ajust marker</div><div class="description">Here is the map <strong>dont know</strong></div></div><div class="extra content"><div class="ui two buttons"><button class="ui green basic button">Edit</button><button class="ui red basic button">do something</button>
+      <div class="ui cards" ><div class="ui card"><div class="content"><img src="#"
+       class="ui mini right floated image"/><div class="header">your current location</div><div class="meta">share with loved ones or rate</div><div class="description">Here is the map <strong>dont know</strong></div></div><div class="extra content"><div class="ui two buttons"><button class="ui green basic button">share</button><button class="ui red basic button">rate</button>
       </div>
       </div></div>
       </div>
