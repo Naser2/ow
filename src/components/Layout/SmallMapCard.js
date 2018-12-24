e1f2b1b838bcf9dd61aab7616315f9856654b296
Nas <nassersanou23@gmail.com>
2018-12-24 02:37:55 -0500

    add small card

--- src/components/Layout/SmallMapCard.js
+++ src/components/Layout/SmallMapCard.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 export default class SmallMapCard extends Component {
   render() {
     return (
-      <div class="ui cards" ><div class="ui card"><div class="content"><img src="#"
+      <div class="ui cards" style={{float:"left", paddingLeft: "1.58571429em"}}><div class="ui card"><div class="content"><img src="#"
        class="ui mini right floated image"/><div class="header">your current location</div><div class="meta">share with loved ones or rate</div><div class="description">Here is the map <strong>dont know</strong></div></div><div class="extra content"><div class="ui two buttons"><button class="ui green basic button">share</button><button class="ui red basic button">rate</button>
       </div>
       </div></div>
       </div>
+      
     )
   }
 }
