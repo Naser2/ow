a143c473bda588779a1e9f7e94b0e46c9d9aeb4c
Nas <nassersanou23@gmail.com>
2018-12-26 04:51:25 -0500

    Pages WIP4

--- src/pages/HomeForm.js
+++ src/pages/HomeForm.js
@@ -10,7 +10,7 @@ export default class HomeForm extends Component {
                   <div className="app-card-content-inner">
                      <div className="upload-profile-photo-main">
                         <label>
-                          <input type="file" multiple={false} name="upload-profile_photo-input" id="upload-profile_photo-input"/>
+                          <input type="file" multiple={true} name="upload-profile_photo-input" id="upload-profile_photo-input"/>
                             <span className="upload-photo-icon"></span>
                             <span className="upload-photo-description">
                               Select or Drag your profile photo here
@@ -30,10 +30,14 @@ export default class HomeForm extends Component {
                 </div>
                 <div className="app-form-item">
                     <label>Email</label>
-                    <input htmlFor="to" type="text" name="name" placeholder="Email" id="email"/>
+                    <input htmlFor="to" type="text" name="Email" placeholder="Email" id="email"/>
+                </div> 
+                
+                 <div className="app-form-actions">
+                    <button type="submit" className="app-button primary">Submit</button>
                 </div>
 
-         <Fragment>
+         {/* <Fragment>
         <div className="app-form-item">
             <div class="field">
             <div class="ui checkbox">
@@ -46,13 +50,13 @@ export default class HomeForm extends Component {
             <div class="ui grid"><div class="eight wide column"><button class="ui primary button">A button that can be focused</button></div><div class="eight wide column"><button class="ui button">Set focused</button></div>
             </div>
         </div> 
-        </Fragment>             
+        </Fragment>              */}
                
                
                 <div className="app-card-content">
-                   Content Header 
+                
                   <div className="app-card-content-inner">
-                     Content Inner
+                 
                   </div>
                 </div>
             </form>
