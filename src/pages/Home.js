b5027c43bc2e46665bff9da67d4de7cb87014da9
Nas <nassersanou23@gmail.com>
2018-12-26 01:22:14 -0500

    Pages WIP1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,21 +5,36 @@ import Header from '../components/Layout/Header';
 export default class Home extends Component {
   render() {
     return (
-      <div>
-       
-        
-          <div className={'app-container'}>
+      <div className="main-pages-div">
+      Temp Main Begin
+       <div className={'app-container'}>
+          Container Begin 
              <Header />
-            <div className={'app-content'}>          
+            <div className={'app-content'}>    
+              Begin App Content
               <div className="app-card">
-              <div className="app-card-header"></div>
+                Card Begin
+                <div className="app-card-header">
+                   Header
+                   <div className="app-card-content-inner">
+                      Header Inner
+                    </div>
+                </div>
+                <div className="app-card-content">
+                   Content Header 
+                  <div className="app-card-content-inner">
+                     Inner
+                  </div>
+                </div>
+                Card End 
               </div>
-            </div>
-
+              End App Content
           </div>
-        
-
-      </div>
+          End App Container    
+       </div>
+      Temp Main End 
+    </div>
+ 
     )
   }
 }
