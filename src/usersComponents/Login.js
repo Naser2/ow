d7d588de8514c16af154430002542329d14f25ff
Nas <nassersanou23@gmail.com>
2018-12-27 14:48:28 -0500

    add Cunstom Login

--- src/usersComponents/Login.js
+++ src/usersComponents/Login.js
@@ -23,100 +23,44 @@ export default class SignupForm extends Component {
 
   render() {
     return (
-      <Fragment>
-      <h1> Hello LOGIN</h1> 
-
-
-      {/* <form class="ui form"><div class="field">
-      
-      
-      
-      </form> */}
-      <div id="loginCardContainer"
-       style={{ position: "relative",
-                width: "100%",
-                minHeight: "1px",
-                // paddingRight: "15px",
-                // paddingLeft: "15px",
-                backgroundColor: "orange" ,
-                display: "flex",
-    'flex-wrap': "wrap",
-    height: "100px",
-    widht: "50px",
-  //  ' margin-right': "-15px",
-  //  ' margin-left': "-15px",
-// }
-//                 'margin-left': "auto!important",
-//                 'margin-right': "auto!important"     
-}}>
-
-
-
-
-<form class="ui small form" >
-<div class="equal width fields"><div class="field"><label>First name</label><input placeholder="First name"/></div>
-
-<div class="field"><label>Last name</label><input placeholder="Last name"/></div>
-<div class="field"></div><label>First name</label><div class="ui input"><input type="text" placeholder="First name"/></div>
-
-</div><div class="field"><div class="ui checkbox"><input type="checkbox" class="hidden" readonly="" tabindex="0"/><label>I agree to the Terms and Conditions</label>
-
-
-
-</div></div><button type="submit" class="ui button">Submit</button>
-
-<div class="ui grid"><div class="eight wide column"><button class="ui primary button">A button that can be focused</button></div><div class="eight wide column"><button class="ui button">Set focused</button></div></div>
-</form>
-
-
-
-
-
-
-
-
-</div>
-    
-      </Fragment>
-    //   <div class="container" style={{position: "relative", padding: "20px 20x",background: "#1273de",
-    //     'boxSizing': "border-box",
-    //     'background-color': "white",
-    //     'boxShadow': "0px 0px 15px black",
-    //     'borderRadius': "5px"}}>
-
-    //   <div className="row"  >
-    //       <div className="col-sm-4"></div>
-    //         <a className="btn big-register" style={{margin: "60px 80x"}} >Login</a>
-    //      </div>
-
-    //      <div className="form AddressBox " style={{ padding: "20px 200px",}}>
-    //     <form onSubmit={this.handleSubmit }>
-
-    //     <input id="phone" style={{overflow: "auto", margin: "12px", 'border-collapse': "collapse"}} className="form-control" type="email" placeholder="Email" name="email"
-    //     value={this.state.phone}
-    //     onChange={ (e)=> this.handleLoginChange(e) }></input>
-        
-    //     <input id="password" style={{overflow: "auto", margin: "12px", 'border-collapse': "collapse"}} className="form-control" type="password" placeholder="Password" name="password"
-    //     value={this.state.password}
-    //     onChange={ (e)=> this.handleLoginChange(e) }></input>
-    //     <input class="btn btn-default btn-login"
-    //      style={{
-    //       'margin-right': "auto",
-    //       'margin-left': "auto",
-    //       display: "block",
-    //       padding: "8px 16px",
-    //       'font-size':" 16px",
-    //       color: "#ffff",
-    //       'backgroundColor': "#fccb00",
-    //       'border': "0",
-    //       'borderRadius': "2px",
-    //       cursor: "pointer",
-    //       transition: "background-color. 15s ease-in",
-    //       'marginTop': "16px" }}
-    //       type="submit" value="Sign" ></input>
-    //      </form>
-    // </div>
-  //  </div>
+      <div className="app-card">
+      <form>
+          <div className="app-card-header">
+            <div className="app-card-content-inner">
+               <div className="upload-profile-photo-main">
+                  <label>
+                       LOGIN
+                      <span className="upload-photo-description">
+                         LOGIN
+                      </span>
+                  </label>
+                </div>
+             </div>
+          </div>  
+          <div className="first-and-lastname">
+              <div  className="app-form-item">
+              {/* <div class="ui form">*/}
+                  <label htmlFor="to">email</label>
+                  <input type="text" placeholder="Email" id="to"/>
+              </div> 
+              <div className="app-form-item">    
+                  <label>password</label>
+                  <input htmlFor="to" type="password" placeholder="Password" id="to"/>
+              </div>
+          </div>
+          
+           <div className="app-form-actions">
+              <button type="submit" className="app-button primary">Submit</button>
+           </div>
+         
+          <div className="app-card-content">
+          
+            <div className="app-card-content-inner">
+           
+            </div>
+          </div>
+      </form>
+    </div>
 
     )
   }
